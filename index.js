b896df5d468f55cf9015c5a591198196d3de4d9c
Styro457 <raduursache6@gmail.com>
2021-12-04 02:41:04 +0200

    Added basic functionality to the generate button

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-function showLayerNames() {
-    const app = window.require("photoshop").app;
-    const allLayers = app.activeDocument.layers;
-    const allLayerNames = allLayers.map(layer => layer.name);
-    const sortedNames = allLayerNames.sort((a, b) => a < b ? -1 : a > b ? 1 : 0);
-    document.getElementById("layers").innerHTML = `
-      <ul>${
-        sortedNames.map(name => `<li>${name}</li>`).join("")
-      }</ul>`;
-}
+document.getElementById("btn-generate-wordart").addEventListener("click", generateWordArt);
 
-document.getElementById("btnPopulate").addEventListener("click", showLayerNames);
+
+async function generateWordArt() {
+    await createRainbowWordArt("WordArt");
+}
